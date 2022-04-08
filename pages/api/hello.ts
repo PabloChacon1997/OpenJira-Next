8b95135b4101ad536b5e3a13d92ad15d20cad0fb
PabloChacon1997 <pabloandres120297@gmail.com>
2022-04-07 20:07:11 -0500

    Conectandonos a la bd

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//mongodb root : XvEiGuuBO99xDnsG
+// mongodb+srv://root:XvEiGuuBO99xDnsG@entries.wg1bo.mongodb.net/test
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  ok: boolean,
+  message: string,
+  method: string,
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
+  console.log(process.env);
+
+  
+  res.status(200).json({
+    ok: true,
+    message: 'Todo ok',
+    method: req.method || 'No hay metodo'
+  });
 }
